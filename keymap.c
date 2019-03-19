474224288f322befee45124ffcff9bdc67759542
Simon Dagfinrud <semonje@gmail.com>
2019-03-20 00:03:47 +0100

    Complete remapping

--- keymap.c
+++ keymap.c
@@ -35,10 +35,16 @@ enum planck_keycodes {
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
+#define Z_LSFT MT(MOD_LSFT, KC_Z)
+#define CPS_LSFT MT(MOD_LSFT, KC_CAPSLOCK)
+#define GRV_RSFT MT(MOD_RSFT, KC_GRV)
 
-#define ALFRED LGUI(KC_SPC)
-#define ZLS MT(MOD_LSFT, KC_Z)
-#define ARS MT(MOD_RSFT, NO_AA)
+#define LPRN S(KC_8)
+#define LBRK S(A(KC_8))
+#define LBRC A(KC_8)
+#define RPRN S(KC_9)
+#define RBRK S(A(KC_9))
+#define RBRC A(KC_9)
 
 // Disable light in middle of 2U position
 #ifdef RGB_MATRIX_H
@@ -51,61 +57,61 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
    * ,-----------------------------------------------------------------------------------.
-   * |  Q   |   W  |   E  |   R  |   T  | Alfr | Caps |   Y  |   U  |   I  |   O  |   P  |
+   * |  Q   |   W  |   E  |   R  |   T  |  Esc | Bksp |   Y  |   U  |   I  |   O  |   P  |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |  A   |   S  |   D  |   F  |   G  |  Esc | Bksp |   H  |   J  |   K  |   L  |   @  |
+   * |  A   |   S  |   D  |   F  |   G  |  Tab | Entr |   H  |   J  |   K  |   L  |   Å  |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Z/Ls |   X  |   C  |   V  |   B  |  Tab | Entr |   N  |   M  |   Æ  |   Ø  | Å/Rs |
+   * | Z/Ls |   X  |   C  |   V  |   B  |   ,  |   .  |   N  |   M  |   Ø  |   Æ  | '/Rs |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Lsft | Ctrl | Alt  | Cmd  |  Lwr |    Space    |  Rse | Left | Down |  Up  | Rght |
+   * | Cp/S | Ctrl | Alt  | Cmd  |  Lwr |    Space    |  Rse | Left | Down |  Up  | Rght |
    * `-----------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT_planck_grid(
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   ALFRED, KC_CAPS, KC_Y,  KC_U,    KC_I,     KC_O,    KC_P,
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_ESC, KC_BSPC, KC_H,  KC_J,    KC_K,     KC_L,    KC_NUHS,
-    ZLS,     KC_X,    KC_C,    KC_V,    KC_B,   KC_TAB, KC_ENT,  KC_N,  KC_M,    NO_AE,    NO_OSLH, ARS,
-    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,  KC_SPC, KC_SPC,  RAISE, KC_LEFT, KC_DOWN,  KC_UP,   KC_RIGHT
+    KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,   KC_ESC,  KC_BSPC, KC_Y,  KC_U,    KC_I,    KC_O,  KC_P,
+    KC_A,     KC_S,    KC_D,    KC_F,    KC_G,   KC_TAB,  KC_ENT,  KC_H,  KC_J,    KC_K,    KC_L,  NO_AA,
+    Z_LSFT,   KC_X,    KC_C,    KC_V,    KC_B,   KC_COMM, KC_DOT,  KC_N,  KC_M,    NO_OSLH, NO_AE, GRV_RSFT,
+    CPS_LSFT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,  KC_SPC,  KC_SPC,  RAISE, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
   ),
 
   /* Lower
    * ,-----------------------------------------------------------------------------------.
-   * |      |      |      |   #  |   &  |      |      |   "  |   (  |   )  |   |  |   +  |
+   * |      |   #  |   &  |      |   {  |   ´  |      |   }  |      |   |  |   +  |      |
    * |------+------+------+------+------+-------------+------+------+------+------+------|
-   * |      |      |      |   %  |   !  |      |      |   ,  |   [  |   ]  |  <>  |   -  |
+   * |      |   %  |   !  |   <  |   (  |      |      |   )  |   >  |   /  |   -  |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |   $  |   ?  |      |      |   .  |   {  |   }  |   ~  |   ´  |
+   * |      |   $  |   ?  |      |   [  |   `  |   "  |   ]  |      |   \  |   ~  |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
    * `-----------------------------------------------------------------------------------'
    */
   [_LOWER] = LAYOUT_planck_grid(
-    _______, _______, _______, KC_HASH, S(KC_6),    _______, _______, S(KC_2), S(KC_8),    S(KC_9),    KC_PIPE, KC_MINS,
-    _______, _______, _______, KC_PERC, KC_EXLM,    _______, _______, KC_DOT,  A(KC_8),    A(KC_9),    KC_NUBS, KC_SLSH,
-    _______, _______, _______, KC_DLR,  S(KC_MINS), _______, _______, KC_COMM, S(A(KC_8)), S(A(KC_9)), KC_TILD, KC_EQL,
-    _______, _______, _______, _______, _______,    _______, _______, _______, KC_MNXT,    KC_VOLD,    KC_VOLU, KC_MPLY
+    _______, KC_HASH, S(KC_6),    _______, LBRK,    KC_EQL,    _______, RBRK,    _______,    A(KC_7),    KC_MINS,  _______,
+    _______, KC_PERC, KC_EXLM,    KC_NUBS, LPRN,    _______,   _______, RPRN,    S(KC_NUBS), S(KC_7),    KC_SLSH,  _______,
+    _______, KC_DLR,  S(KC_MINS), _______, LBRC,    S(KC_EQL), S(KC_2), RBRC,    _______,    S(A(KC_7)), KC_TILDE, _______,
+    _______, _______, _______,    _______, _______, _______,   _______, _______, KC_MNXT,    KC_VOLD,    KC_VOLU,  KC_MPLY
   ),
 
   /* Raise
    * ,-----------------------------------------------------------------------------------.
    * |   0  |   1  |   2  |   3  |      |      |      |      |      |      |      |      |
    * |------+------+------+------+------+-------------+------+------+------+------+------|
-   * |   +  |   4  |   5  |   6  |   /  |      |      |      |      |      |      |      |
+   * |      |   4  |   5  |   6  |      |      |      | Left | Down |  Up  | Rght |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |   -  |   7  |   8  |   9  |      |      |      |      |      | PgUp | PgDn |      |
+   * |      |   7  |   8  |   9  |      |      |      |      |      |      |      |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+   * |      |      |      |      |      |             |      | Prev | Bri- | Bri+ | Play |
    * `-----------------------------------------------------------------------------------'
    */
   [_RAISE] = LAYOUT_planck_grid(
-    KC_0,    KC_1,    KC_2,    KC_3,    _______, _______, _______, _______, _______, _______, _______, _______,
-    KC_PLUS, KC_4,    KC_5,    KC_6,    _______, _______, _______, _______, _______, _______, _______, _______,
-    KC_SLSH, KC_7,    KC_8,    KC_9,    _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+    KC_0,    KC_1,    KC_2,    KC_3,    _______, _______, _______, _______, _______, _______, _______,  _______,
+    _______, KC_4,    KC_5,    KC_6,    _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______,
+    _______, KC_7,    KC_8,    KC_9,    _______, _______, _______, _______, _______, _______, _______,  _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_SLCK, KC_PAUS,  KC_MPLY
   ),
 
   /* Adjust (Lower + Raise)
    * ,-----------------------------------------------------------------------------------.
-   * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
+   * | Reset|      |      |      |      |      |      |      |      |      |      |  Del |
    * |------+------+------+------+------+-------------+------+------+------+------+------|
    * |      |      |      |Aud on|Audoff|AGnorm|AGswap|      |      |      |      |      |
    * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -115,10 +121,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------------------------------'
    */
   [_ADJUST] = LAYOUT_planck_grid(
-    _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
-    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______,  _______,  _______, _______,  _______,
-    _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
+    RESET,   DEBUG,   RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD,  KC_F1,  KC_F2,  KC_F3,
+    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______,  KC_F4,  KC_F5,  KC_F6,
+    _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, KC_F7,  KC_F8,  KC_F9,
+    _______, _______, _______, _______, _______, RGB_TOG, RGB_TOG, _______, _______,  KC_F10, KC_F11, KC_F12
   )
 
 };
