d1e2761f30342ac0e3c9b3279a1c5d8717c98723
Simon Dagfinrud <17279882+kvasimon@users.noreply.github.com>
2021-02-01 19:26:26 +0100

    Update EZ to work with Dutch layout

--- ez/keymap.c
+++ ez/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT_planck_grid(
-    KC_GRV,   KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_NUBS,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,   KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    NO_AA,   KC_ENT,
     TAB_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  NO_OSLH, NO_AE,   KC_COMM, KC_DOT,
     CPS_LSFT, KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_LOWER] = LAYOUT_planck_grid(
     _______, KC_HASH, S(KC_6),    _______, LBRK,    KC_EQL,    S(KC_0), RBRK,    _______,    A(KC_7),    NO_CIRC, _______,
-    _______, KC_PERC, KC_EXLM,    KC_NUBS, LPRN,    NO_ASTR,   KC_BSLS, RPRN,    S(KC_NUBS), S(KC_7),    KC_RBRC, _______,
+    _______, KC_PERC, KC_EXLM,    KC_GRV,  LPRN,    NO_ASTR,   KC_BSLS, RPRN,    S(KC_GRV),  S(KC_7),    KC_RBRC, _______,
     _______, KC_DLR,  S(KC_MINS), _______, LBRC,    S(KC_EQL), S(KC_2), RBRC,    _______,    S(A(KC_7)), NO_TILD, _______,
     _______, _______, _______,    _______, _______, _______,   _______, _______, KC_MNXT,    KC_VOLD,    KC_VOLU, KC_MPLY
   ),
